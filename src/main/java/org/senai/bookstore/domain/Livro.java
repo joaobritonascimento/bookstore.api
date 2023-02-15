8807cfc156425542545d3e53920a10e0c3c0277e
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-15 01:56:59 -0300

    Criação das classes Categoria e Livros

---
+++ src/main/java/org/senai/bookstore/domain/Livro.java
@@ -0,0 +1,86 @@
+package org.senai.bookstore.domain;
+
+public class Livro {
+    private Integer id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    private Categoria categoria;
+
+    public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
+        super ();
+        this.id = id;
+        this.titulo = titulo;
+        this.nome_autor = nome_autor;
+        this.texto = texto;
+        this.categoria = categoria;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNome_autor() {
+        return nome_autor;
+    }
+
+    public void setNome_autor(String nome_autor) {
+        this.nome_autor = nome_autor;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Livro other = (Livro) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
