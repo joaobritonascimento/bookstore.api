3c0d8b0320d2d482128d254be701a1508f63e1e7
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-19 17:16:18 -0300

    3

--- src/main/java/org/senai/bookstore/domain/Categoria.java
+++ src/main/java/org/senai/bookstore/domain/Categoria.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-//import javax.persistence.Entity;
+import javax.persistence.Entity;
 //import javax.persistence.GeneratedValue;
 //import javax.persistence.GenerationType;
 //import javax.persistence.Id;
