6561170d9ed5a405e416bce23f6dc9cb0579ba0e
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-19 19:58:50 -0300

    Aula 07 - configuração do application properties - criação do bd_bookstore

--- src/main/java/org/senai/bookstore/BookstoreApplication.java
+++ src/main/java/org/senai/bookstore/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package org.senai.bookstore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
+import org.senai.bookstore.domain.Categoria;
+import org.senai.bookstore.domain.Livro;
+import org.senai.bookstore.repositores.CategoriaRepository;
+import org.senai.bookstore.repositores.LivroRepository;
+
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+    @Autowired
+	private LivroRepository livroRepository;
+
+	public static void main(String[] args){
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override	
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
+	
 }
+
+
