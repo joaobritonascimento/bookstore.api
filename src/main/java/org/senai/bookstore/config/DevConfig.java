581b2149b448dd659637f186a97df40752e2bd45
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-21 00:52:41 -0300

    Aula 09 - Criação do perfil dev e atualização das dependencias - mysql

---
+++ src/main/java/org/senai/bookstore/config/DevConfig.java
@@ -0,0 +1,31 @@
+package org.senai.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import org.senai.bookstore.Service.DBService;
+
+@Configuration
+@Profile("dev") //Nome do perfil dev
+public class DevConfig {
+    
+    @Autowired
+    private DBService dbService;
+
+    //o value pega o caminho/valor que esta entre chaves
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciaBaseDeDados(){
+        if(strategy.equals("create")){
+            this.dbService.instanciaBaseDeDados();
+        }
+        return false;
+        
+    }
+    }
+
