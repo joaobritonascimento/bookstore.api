82514a8b1de3f8294ffee2a9a6c17253c1124c9a
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-20 23:22:46 -0300

    configuração configtest

--- src/main/java/org/senai/bookstore/repositores/CategoriaRepository.java
+++ src/main/java/org/senai/bookstore/repositores/CategoriaRepository.java
@@ -1,7 +1,7 @@
 package org.senai.bookstore.repositores;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import org.senai.bookstore.domain.Categoria;
 
