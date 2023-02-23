f5925cd5e8ead869378346263ca5e7b601c19db5
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-23 19:40:53 -0300

    Aula 20 - POST para Livros

--- src/main/java/org/senai/bookstore/Service/LivroService.java
+++ src/main/java/org/senai/bookstore/Service/LivroService.java
@@ -3,11 +3,13 @@ package org.senai.bookstore.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.senai.bookstore.domain.Livro;
 import org.senai.bookstore.repositories.LivroRepository;
 import org.senai.bookstore.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import org.senai.bookstore.domain.Categoria;
+import org.senai.bookstore.domain.Livro;
 
 @Service
 public class LivroService {
@@ -40,6 +42,13 @@ public class LivroService {
         newObj.setTitulo(obj.getTitulo());
         newObj.setNome_autor(obj.getNome_autor());
         newObj.setTexto(obj.getTexto());
+    }
+
+    public Livro create(Integer id_cat, Livro obj) {
+        obj.setId(null);
+        Categoria cat = categoriaService.findById(id_cat);
+        obj.setCategoria(cat);
+        return repository.save(obj);
     }
 
     
--- src/main/java/org/senai/bookstore/resources/LivroResource.java
+++ src/main/java/org/senai/bookstore/resources/LivroResource.java
@@ -1,7 +1,10 @@
 package org.senai.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.servlet.Servlet;
 
 import org.senai.bookstore.domain.Livro;
 import org.senai.bookstore.dtos.LivroDTO;
@@ -11,11 +14,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -48,4 +53,14 @@ public class LivroResource {
         Livro newObj = service.update(id, obj);
         return ResponseEntity.ok().body(newObj);
     }
+
+    @PostMapping
+    public ResponseEntity<Livro> create(@RequestParam(value="categoria", defaultValue = "0") Integer id_cat,
+    @RequestBody Livro obj){
+        Livro newObj = service.create(id_cat, obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+
 }
