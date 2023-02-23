79a71b34bd6c53bdbfd236e88580e6d05e245415
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-23 01:50:15 -0300

    4

--- src/main/java/org/senai/bookstore/repositories/LivroRepository.java
+++ src/main/java/org/senai/bookstore/repositories/LivroRepository.java
@@ -1,6 +1,6 @@
 package org.senai.bookstore.repositories;
 
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +9,7 @@ import java.util.List;
 
 import org.senai.bookstore.domain.Livro;
 
-@Repository
+
 public interface LivroRepository extends JpaRepository<Livro, Integer>{
 
     @Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY titulo")    
