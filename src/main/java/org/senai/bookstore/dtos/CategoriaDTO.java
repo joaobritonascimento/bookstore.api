6a43e2c27bcafc48530afb4c02fed9540f68de45
joaobritonascimento <joaobritonascimento@gmail.com>
2023-02-27 21:57:21 -0300

    Inserção de comentario sobre implements

--- src/main/java/org/senai/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/org/senai/bookstore/dtos/CategoriaDTO.java
@@ -7,46 +7,53 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 import org.senai.bookstore.domain.Categoria;
 
-public class CategoriaDTO implements Serializable{
+public class CategoriaDTO implements Serializable { // a classe Serializable é uma interface em metodos ou atributos
 
-    private static final long serialVersionUID=1l;
+    private static final long serialVersionUID = 1l;
 
     private Integer id;
-    
+
     @NotEmpty(message = "Campo NOME é obrigatório")
-    @Length(min =3, max = 100, message = "O campo NOME de deve entre 3 e 100 caracteres")
+    @Length(min = 3, max = 100, message = "O campo NOME de deve entre 3 e 100 caracteres")
     private String nome;
 
     @NotEmpty(message = "Campo DESCRIÇÃO é obrigatório")
-    @Length(min =3, max = 200, message = "O campo DESCRIÇÃO de deve entre 3 e 200 caracteres")
+    @Length(min = 3, max = 200, message = "O campo DESCRIÇÃO de deve entre 3 e 200 caracteres")
     private String descricao;
 
-    public CategoriaDTO(){
+    public CategoriaDTO() {
         super();
     }
+
     public CategoriaDTO(Categoria obj) {
         super();
         this.id = obj.getId();
         this.nome = obj.getNome();
         this.descricao = obj.getDescricao();
     }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getDescricao() {
         return descricao;
     }
+
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-    
+
 }
