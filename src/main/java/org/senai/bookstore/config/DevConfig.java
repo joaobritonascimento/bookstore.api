37f62f00919b32cf2dfbe092897e31b5df048887
joaobritonascimento <joaobritonascimento@gmail.com>
2023-03-16 02:41:59 -0300

    1

--- src/main/java/org/senai/bookstore/config/DevConfig.java
+++ src/main/java/org/senai/bookstore/config/DevConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+
 @Configuration
 @Profile("dev") //Nome do perfil dev
 public class DevConfig {
--- src/main/java/org/senai/bookstore/config/TestConfig.java
+++ src/main/java/org/senai/bookstore/config/TestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+
 @Configuration
 @Profile("test") //Nome do perfil test
 public class TestConfig {
